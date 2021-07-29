b5fc29e2020ac732322873fee93352dea03aa90c
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-07-30 04:12:07 +0530

    list

--- setup/src/List.js
+++ setup/src/List.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
+import data from "./data";
 
 const List = () => {
+  const [values, setValues] = useState(data);
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <div>
+      {values.map((people) => {
+        return (
+          <div key={people.id}>
+            <img src={people.image}></img>
+            <h3>{people.name}</h3>
+            <small>{people.age}</small>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
